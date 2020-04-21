2fa6a3a8a09d08c0cb0930ba40f886b262bcaf2c
Michael Olatunji <tundexmike@gmail.com>
2020-04-21 19:09:52 +0100

    initial commit

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CakeContainer = () => {
+    return (
+        <div>
+            <h3>Number of Cake</h3>
+
+            <button>Buy Cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer;
